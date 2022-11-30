22b593af20a851625e862c6248a1656f355df108
muluberhanww <muluberhanw16@gmail.com>
2022-11-30 12:31:16 -0800

    fixed

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -9,11 +9,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
-
-    current = h;
-    n = 0;
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
+       
+	current = h;
+	n = 0;
     while (current != NULL)
     {
         printf("%i\n", current->n);
--- 0x01-python-if_else_loops_functions/main.c
+++ 0x01-python-if_else_loops_functions/main.c
@@ -10,28 +10,25 @@
  */
 int main(void)
 {
-    listint_t *head;
+	listint_t *head;
+	head = NULL;
 
-    head = NULL;
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 5);
-    add_nodeint_end(&head, 7);
-    add_nodeint_end(&head, 13);
-    add_nodeint_end(&head, 24);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-
-    printf("-----------------\n");
-
-    insert_node(&head, 7465);
-    insert_node(&head, 619);
-    insert_node(&head, 2000);
-
-    print_listint(head);
-
-    free_listint(head);
-
-    return (0);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 5);
+	add_nodeint_end(&head, 7);
+	add_nodeint_end(&head, 13);
+	add_nodeint_end(&head, 24);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint(head);
+	printf("-----------------\n");
+	
+	insert_node(&head, 7465);
+	insert_node(&head, 619);
+	insert_node(&head, 2000);
+	print_listint(head);
+	free_listint(head);
+	
+	return (0);
 }
